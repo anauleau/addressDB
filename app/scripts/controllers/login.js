00d5d38cced81763bf09a6781a9afee9add18a7a
Andreas Nauleau <anauleau@gmail.com>
2015-12-19 20:25:52 -0800

    structures data models for users and addresses and adds proper api interaction

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,7 +7,9 @@
  * Manages authentication to any active providers.
  */
 angular.module('addressDbApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location, $q, Ref, $timeout, $routeParams) {
+  .controller('LoginCtrl', function ($scope, Auth, $location, $q, Ref, $timeout, $routeParams, States) {
+    $scope.model = {};
+    $scope.states = States;
     $scope.createMode = $routeParams.register === 'true' ? true : false;
 
     $scope.oauthLogin = function(provider) {
@@ -27,33 +29,87 @@ angular.module('addressDbApp')
       );
     };
 
-    $scope.createAccount = function(email, pass, confirm) {
+    $scope.createAccount = function(params, confirm) {
+      var userId;
       $scope.err = null;
-      if( !pass ) {
+      if( !params.pass ) {
         $scope.err = 'Please enter a password';
       }
-      else if( pass !== confirm ) {
+      else if( params.pass !== confirm ) {
         $scope.err = 'Passwords do not match';
       }
       else {
-        Auth.$createUser({email: email, password: pass})
+        Auth.$createUser({email: params.email, password: params.pass})
           .then(function () {
             // authenticate so we have permission to write to Firebase
-            return Auth.$authWithPassword({email: email, password: pass}, {rememberMe: true});
+            return Auth.$authWithPassword({email: params.email, password: params.pass}, {rememberMe: true});
           })
           .then(createProfile)
+          .then(createAddress)
+          .then(addAddressHook)
           .then(redirect, showError);
       }
 
       function createProfile(user) {
-        var ref = Ref.child('users', user.uid), def = $q.defer();
-        ref.set({email: email, name: firstPartOfEmail(email)}, function(err) {
+        var ref = Ref.child('users'), def = $q.defer(), newProfileObj = {};
+            newProfileObj[user.uid] = {
+                id: user.uid,
+                email: params.email,
+                name: firstPartOfEmail(params.email),
+                firstName: params.firstName,
+                lastName: params.lastName
+            };
+        ref.set(newProfileObj, function(err) {
           $timeout(function() {
             if( err ) {
               def.reject(err);
             }
             else {
-              def.resolve(ref);
+              userId = user.uid;
+              def.resolve(user);
+            }
+          });
+        });
+        return def.promise;
+      }
+
+      function createAddress(user) {
+        var ref = Ref.child('addresses'), def = $q.defer(), users = {}, newAddress;
+        users[user.uid] = true;
+        newAddress = ref.push({
+            createdAt: new Date(),
+            name: params.addressName,
+            email: params.email,
+            address1: params.address1,
+            address2: params.address2,
+            city: params.city,
+            state: params.state,
+            postalCode: params.postalCode,
+            country: params.country,
+            users: users
+            }, function(err) {
+          $timeout(function() {
+            if( err ) {
+              def.reject(err);
+            }
+            else {
+              def.resolve(newAddress.key());
+            }
+          });
+        });
+        return def.promise;
+      }
+
+      function addAddressHook(addressId) {
+          var ref = Ref.child('users/' + userId + '/addresses'), def = $q.defer(), hook = {};
+          hook[addressId] = true;
+          ref.set(hook, function(err) {
+          $timeout(function() {
+            if( err ) {
+              def.reject(err);
+            }
+            else {
+              def.resolve();
             }
           });
         });
---
+++ app/scripts/services/states.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name addressDbApp.States
+ * @description
+ * # States
+ * Factory in the addressDbApp.
+ */
+angular.module('addressDbApp')
+  .factory('States', function () {
+    var states = {
+        'AL': 'Alabama',
+        'AK': 'Alaska',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'AR': 'Arkansas',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DE': 'Delaware',
+        'DC': 'District Of Columbia',
+        'FM': 'Federated States Of Micronesia',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'IA': 'Iowa',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'ME': 'Maine',
+        'MH': 'Marshall Islands',
+        'MD': 'Maryland',
+        'MA': 'Massachusetts',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MS': 'Mississippi',
+        'MO': 'Missouri',
+        'MT': 'Montana',
+        'NE': 'Nebraska',
+        'NV': 'Nevada',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NY': 'New York',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'MP': 'Northern Mariana Islands',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PW': 'Palau',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VT': 'Vermont',
+        'VI': 'Virgin Islands',
+        'VA': 'Virginia',
+        'WA': 'Washington',
+        'WV': 'West Virginia',
+        'WI': 'Wisconsin',
+        'WY': 'Wyoming'
+    };
+
+    // Public API here
+    return states;
+  });
