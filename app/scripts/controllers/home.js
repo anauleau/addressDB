ff19056f970fb8e47abe14f3c2b21ca71fc78bd1
Andreas Nauleau <anauleau@gmail.com>
2015-12-21 18:56:14 -0800

    adds basic home page

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -9,21 +9,10 @@
  */
 angular.module('addressDbApp')
   .controller('HomeCtrl', function ($scope, user, Auth, Ref, $firebaseObject) {
-    $scope.user = user;
     $scope.logout = function() { Auth.$unauth(); };
-    $scope.addresses = [];
-    $scope.userAddress = $firebaseObject(Ref);
-    var profileRef = Ref.child('users/' + user.uid + '/addresses');
-    var addressList = $firebaseObject(profileRef);
-    $scope.addressList = [];
-    
-    addressList.$loaded().then(function () {
-       angular.forEach(addressList, function (val, id){
-           var addressRef = Ref.child('addresses/' + id);
-           var address = $firebaseObject(addressRef);
-           address.$loaded().then(function (){
-              $scope.addressList.push(address);
-           });
-       });
+    var profileRef = Ref.child('users/' + user.uid);
+    $scope.profile = $firebaseObject(profileRef);
+    $scope.profile.$loaded(function (data){
+        $scope.address = $firebaseObject(Ref.child('addresses/' + Object.keys(data.addresses)[0]));
     });
   });
