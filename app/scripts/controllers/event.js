01810fb28caf431de4c2bd2fe26573dc9be72121
Andreas Nauleau <anauleau@gmail.com>
2015-12-26 18:46:30 -0800

    adds additional updates to event ctrl

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -9,7 +9,8 @@
  * Controller of the addressDbApp
  */
 angular.module('addressDbApp')
-  .controller('EventCtrl', function ($scope, Ref, event, invitations, $timeout) {
+  .controller('EventCtrl', function ($scope, Ref, event, invitations, user, $timeout, $q, $firebaseObject) {
+     $scope.userProfile = $firebaseObject(Ref.child('users/' + user.uid));
      $scope.model = {};
      $scope.show = false;
      $scope.event = event;
@@ -18,21 +19,58 @@ angular.module('addressDbApp')
      });
      $scope.invitations = invitations;
      $scope.sendRequest = function (params) {
-         var ref = Ref.child('events/' + $scope.event.$id + '/invitations');
-         ref.push({
-            createdAt: Firebase.ServerValue.TIMESTAMP,
-            name: params.name,
-            email: params.email,
-            addressReceived: false
-         }, function(err) {
-          $timeout(function() {
-            if( err ) {
-              console.log(err);
-            }
-            else {
-              $scope.model = {};
-            }
-          });
-        });
+         // create invite in event
+         function createInvite () {
+             var ref = Ref.child('events/' + $scope.event.$id + '/invitations'),
+                def = $q.defer(),
+                invite;
+            invite = ref.push({
+                createdAt: Firebase.ServerValue.TIMESTAMP,
+                name: params.name,
+                email: params.email,
+                addressReceived: false
+            }, function(err) {
+                $timeout(function() {
+                    if( err ) {
+                        def.reject(err);
+                    }
+                    else {
+                        def.resolve(invite.key());
+                    }
+                });
+            });
+            return def.promise;
+         }
+         function addInvite(id) {
+            var ref = Ref.child('/invitations/' + id),
+                def = $q.defer();
+            ref.update({
+                createdAt: Firebase.ServerValue.TIMESTAMP,
+                owner: event.owner,
+                name: params.name,
+                email: params.email,
+                id: id,
+                ownerName: $scope.userProfile.firstName,
+                eventID: event.id,
+                sent: false,
+                addressReceived: false
+            }, function(err) {
+                $timeout(function() {
+                    if( err ) {
+                        def.reject(err);
+                    }
+                    else {
+                        def.resolve();
+                    }
+                });
+            });
+            return def.promise;
+         }
+         function clearInputs() {
+             $scope.model = {};
+         }
+       createInvite()
+            .then(addInvite)
+            .then(clearInputs); 
      };
   });
